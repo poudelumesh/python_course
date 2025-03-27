cb49d661b3be705bd9d6bb617f583cb640f35186
poudelumesh <poudelumesh7777777@gmail.com>
2025-03-27 11:29:23 +0000

    hi

--- restaurant_bill.py
+++ restaurant_bill.py
@@ -1,7 +1,26 @@
-print("1. Burger - $10")
-print("2. Pizza - $12")
-print("3. Sushi - $15")
+print("please select a menu")
+print("Burger: $10 - press 1")
+print("Burger: $12 - press 2")
+print("Burger: $15 - press 3")
 
-choice = input("enter the number of the item you want to order: ")
+choice = input("please enter the menu number")
+
+number_guest = int(input("please enter the number of guest: "))
+
 if choice == "1":
-    
+    total_price = number_guest * 10
+elif choice == "2":
+    total_price = number_guest * 12
+elif choice == "3":
+    total_price = number_guest * 15
+
+print(f"the total without discount is ${total_price}")       
+
+if total_price >= 200:
+    total_with_discount = total_price - total_price * 0.2
+    print(f"the total with discount is ${total_with_discount}")  
+
+elif  total_price >= 100:
+    total_with_discount = total_price - total_price * 0.1
+    print(f"the total with discount is ${total_with_discount}")  
+   
