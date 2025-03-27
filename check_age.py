30555d04b0cbaaf551178d10e91c0ada4598009d
poudelumesh <poudelumesh7777777@gmail.com>
2025-03-27 10:15:17 +0000

    hello

---
+++ check_age.py
@@ -0,0 +1,7 @@
+name = input("your name: ")
+age =  int(input("your age: "))
+
+if (age < 18):
+    print("Our game is not suitable for players under the age of 18")
+else:
+    print(f"welcome to our website {name} ")               
---
+++ ticket.py
@@ -0,0 +1,8 @@
+age = int(input("your age: "))
+
+if (age >= 18):
+    print("$10 for user")
+elif (age >=4 and age < 18):
+    print("$8 for child")
+else:
+    print("$5 for children")    
