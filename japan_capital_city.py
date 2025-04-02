5936880f2b37d02923d016aec56ff8f726b9e12e
poudelumesh <poudelumesh7777777@gmail.com>
2025-04-02 18:30:43 +0000

    hi

--- japan_capital_city.py
+++ japan_capital_city.py
@@ -1,7 +1,18 @@
-answer = "tokyo"
-max_attempts = 5
-attempts = 0
 
-while attempts < max_attempts:
-    user_answer = input("what is the capital city of japan?: ")
-    
+answer = "Tokyo"
+attempts = 0
+max_attempts = 5
+
+while True:
+    user_answer = input("What is the capital of Japan?: ")
+    attempts += 1  
+
+    if user_answer == answer:  
+        print("Correct answer.")
+        break  
+
+    if attempts == max_attempts:  
+        print("Sorry, the correct answer is Tokyo.")  
+        break  
+
+    print(f"Incorrect. Attempts: {attempts}/{max_attempts}")
