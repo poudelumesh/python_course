fab766e2e18a74b08caf41c96a9531f44deca214
poudelumesh <poudelumesh7777777@gmail.com>
2025-04-02 10:25:17 +0000

    hi

---
+++ list2.py
@@ -0,0 +1,11 @@
+games = []
+while True:
+    game = input("Enter your favorite game: ")
+    if game == "":
+        break
+    else:
+        games.append(game)
+
+print("your favorite games: ")   
+for game in games:
+    print("-", game) 
