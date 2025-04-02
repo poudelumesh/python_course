fda81fe0f623b99dcb623502902b81bc5ef096ee
poudelumesh <poudelumesh7777777@gmail.com>
2025-04-02 10:01:57 +0000

    hello

--- list.py
+++ list.py
@@ -1,5 +1,5 @@
 countries = ["Nepal", "Japan", "Bhutan"]
-print("second country", countries)
+print("second country", countries[1])
 countries.append("Sri lanka")
 remove_country = countries.pop(0)
 for country in countries:
