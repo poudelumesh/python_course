de0ca09f2420c822ef7f3fa67507c3f3e9603ff8
poudelumesh <poudelumesh7777777@gmail.com>
2025-04-09 09:40:41 +0000

    hi

--- shop.py
+++ shop.py
@@ -3,7 +3,7 @@ products = [{"name": "one plus","price": 300},
              {"name": "headphone", "price": 50},
               {"name": "keyboard", "price": 40},]
 
-delivery_company = [{"name": "uber", "price": 15},
+careers = [{"name": "uber", "price": 15},
                      {"name": "pathao", "price": 10},
                      {"name": "foodmandu", "price": 8},
                      {"name": "indrive", "price": 7},]
@@ -20,5 +20,23 @@ while True:
         else:
            print("This product does not exit")
     except ValueError:
-        print("please enter only number")     
+        print("please enter only number")
+
 user_product = product[user_product_index]
+
+
+for index, career in enumerate(careers):
+    print(f"for {career["name"]}press {index}")
+
+while True:
+    user_career_index = int(input("please enter the career number: ")) 
+    try:
+        user_career_index = int("user_career_index")
+        if user_career_index >= 0 and user_career_index < len(careers):
+         break  
+        else:
+           print("This career does not exit")
+    except ValueError:
+        print("please enter only number")
+             
+user_career = career[user_career_index]
